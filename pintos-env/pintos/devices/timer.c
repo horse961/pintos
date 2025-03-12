d3c82fcf90f4dd57c6aa68f0215dddc8d0e58be2
Ruben Oktay <ruben.oktay@usi.ch>
2025-03-12 12:12:42 +0100

    Removed list_init from timer_init, list initialized elsewhere

--- pintos-env/pintos/devices/timer.c
+++ pintos-env/pintos/devices/timer.c
@@ -35,7 +35,6 @@ static void real_time_delay (int64_t num, int32_t denom);
 void
 timer_init (void) 
 {
-  list_init(&sleep_list);
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
